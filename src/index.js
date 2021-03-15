5188a95b83e5bef2528ffa5de8a5aa6d660087a9
dimo12317 <79600828+dimo12317@users.noreply.github.com>
2021-03-15 15:34:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let yes = 0;
+  let bracketsConfigString = "";
+  for(let i = 0; i < bracketsConfig.length; i++){
+      bracketsConfigString += bracketsConfig[i][0];
+      bracketsConfigString += bracketsConfig[i][1];
+  }
+  str +=bracketsConfigString;
+  let strparam  = str.split("");
+  let i = 0;
+  for(let y = 0;y < strparam.length;y++){
+      if(strparam[y] === "(" && strparam[y+1] === ")"){
+          strparam.splice(y,2);
+          y = -1;
+      }
+      else if(strparam[y] === "[" && strparam[y+1] === "]"){
+          strparam.splice(y,2);
+          y = -1;
+      }
+      else if(strparam[y] === "{" && strparam[y+1] === "}"){
+          strparam.splice(y,2);
+          y = -1;
+      }
+      else if(strparam[y] === "|" && strparam[y+1] === "|"){
+          strparam.splice(y,2);
+          y = -1;
+      }
+  }
+  if(strparam[0] === undefined)
+  return true;
+  return false;
 }
