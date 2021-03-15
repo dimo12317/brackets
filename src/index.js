7b6ad43c4dedb2ecd52f076eded373969256305d
dimo12317 <79600828+dimo12317@users.noreply.github.com>
2021-03-15 15:54:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  let yes = 0;
-  let bracketsConfigString = "";
-  for(let i = 0; i < bracketsConfig.length; i++){
-      bracketsConfigString += bracketsConfig[i][0];
-      bracketsConfigString += bracketsConfig[i][1];
-  }
-  str +=bracketsConfigString;
-  let strparam  = str.split("");
-  let i = 0;
-  for(let y = 0;y < strparam.length;y++){
-      if(strparam[y] === "(" && strparam[y+1] === ")"){
-          strparam.splice(y,2);
-          y = -1;
-      }
-      else if(strparam[y] === "[" && strparam[y+1] === "]"){
-          strparam.splice(y,2);
-          y = -1;
-      }
-      else if(strparam[y] === "{" && strparam[y+1] === "}"){
-          strparam.splice(y,2);
-          y = -1;
-      }
-      else if(strparam[y] === "|" && strparam[y+1] === "|"){
-          strparam.splice(y,2);
-          y = -1;
-      }
-  }
-  if(strparam[0] === undefined)
-  return true;
-  return false;
+let result = [];
+let existBracket = {};
+for(let j in bracketsConfig) {
+existBracket[bracketsConfig[j][0]] = bracketsConfig[j][1];
+}
+
+result.push(str.charAt(0));
+
+for(let i = 1; i < str.length; i += 1) {
+if(str.charAt(i) === existBracket[result[result.length - 1]]) {
+result.pop();
+} else {
+result.push(str.charAt(i));
+}
+}
+if (result.length > 0) return false;
+else return true;
 }
